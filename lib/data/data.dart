2516aaa1855a8cb6c667fa301c3cb41a26b59117
Ezra Ariwomoi <kropezra@gmail.com>
2024-07-21 00:01:01 +0300

    add

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 List<Map<String, dynamic>> transactionsData = [
-  
+  {
+    'icon': const FaIcon(FontAwesomeIcons.burger, color: Colors.white),
+    'color': Colors.yellow[700],
+    'name': 'Food',
+    'totalAmount': '-\$45.00',
+    'date': 'Today',
+  },
   {
     'icon': const FaIcon(FontAwesomeIcons.bagShopping, color: Colors.white),
     'color': Colors.purple,
